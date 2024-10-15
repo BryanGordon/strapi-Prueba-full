659e0fd2c845287bd631597d69f0a62932404835
Bryan Gordon <bryanbge92@gmail.com>
2024-10-14 19:27:38 -0500

    Update main page

--- store-web/src/app/page.tsx
+++ store-web/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
+          src=""
           alt="Next.js logo"
           width={180}
           height={38}
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
@@ -88,7 +88,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
+            src=""
             alt="Globe icon"
             width={16}
             height={16}
