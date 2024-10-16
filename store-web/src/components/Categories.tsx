b2c34e2ce625d8a9906cbc4bde8a5f7e700f92b8
Bryan Gordon <bryanbge92@gmail.com>
2024-10-16 18:32:51 -0500

    Update categories component

--- store-web/src/components/Categories.tsx
+++ store-web/src/components/Categories.tsx
@@ -1,7 +1,19 @@
+import Link from 'next/link'
+
 export const Categories = async () => {
+  const categories = []
+
+  if (categories.length === 0) return null
+
   return (
-    <div>
+    <section id='categories' className='bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-16'>
+      <div className='mx-auto max-w-screen-xl px-4 2xl:px-0'>
+        <div>
+          
+        </div>
+
+      </div>
       Categories component
-    </div>
+    </section>
   )
 }
