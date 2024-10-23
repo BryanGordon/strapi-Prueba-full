c51e2aa5d65abfa3c3f15b775f72e5a119e530ce
Bryan Gordon <bryanbge92@gmail.com>
2024-10-22 19:54:25 -0500

    Resolve image render issue

--- store-web/src/lib/get-products.ts
+++ store-web/src/lib/get-products.ts
@@ -3,13 +3,12 @@ import { query } from './strapi';
 const { HOST } = process.env
 
 export function getProducts ({ categoryId }: { categoryId: string }) {
-  return query(`products?filters[product_category][slug][$contains]=${categoryId}`)
+  return query(`products?filters[product_category][slug][$contains]=${categoryId}&populate=images`)
     .then(res => {
       const { data, meta } = res
-
       const products = data.map(product => {
-        const { name, slug, description, image: rawImage, price } = product
-        const image = `${HOST}/${rawImage.url}`
+        const { name, slug, description, images: rawImages, price } = product
+        const image = `${HOST}/${rawImages[0].url}`
         return { name, slug, description, image, price }
       })
 
